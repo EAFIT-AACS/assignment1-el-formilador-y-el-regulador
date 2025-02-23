dab566dfc68c5007cb4803bf91b264ed0d40a5fd
Jeronimo Campuzano <jcampuzanc@eafit.edu.co>
2025-02-22 20:41:41 -0500

    Firts step developed

--- DFA.cpp
+++ DFA.cpp
@@ -1,46 +1,79 @@
-#include <list>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+#include <utility>
 
 using namespace std;
 
 class DFA {
     private:
-        list<string> states;
-        list<string> finals;
-        list<char> alphabet;
-        list<string> equivalentStates;
+        vector<string> states;
+        vector<string> finals;
+        vector<char> alphabet;
+        vector<pair<string, string>> equivalentStates;
+        
     
-    public:
-        void setStates() {
-            
+    public: 
+        DFA(vector<string> q, vector<string> f, vector<char> sigma) {
+            states = q;
+            finals = f;
+            alphabet = sigma;
         }
-
-        void setFinals() {
-
-        }
-
-        void setAlphabet() {
-
-        }
-
-        void setEquivalentStates() {
-
+      
+        
+        /*
+        void addState(string state) {
+            states.push_back(state);
         }
         
-        list<string> getStates() {
+        void addFinal(string final) {
+            finals.push_back(final);
+        }
+
+        void addAlphabet(char letter) {
+            alphabet.push_back(letter);
+        } 
+        */ 
+
+        vector<string> getStates() {
             return states;
         }
         
-        list<string> getFinals() {
+        vector<string> getFinals() {
             return finals;
         }
 
-        list<char> getAlphabet() {
+        vector<char> getAlphabet() {
             return alphabet;
         }
 
-        list<string> getEquivalentStates() {
+        /*
+        vector<string> getEquivalentStates() {
             return equivalentStates;
         }
-              
+        */
+
+        void findEquivalentStates() {
+            vector<string, string> statePairs;
+
+            // Paso 0: Crear todas las parejas posibles entre estados
+            for (auto iterador1 = states.begin(); iterador1 != states.end(); ++iterador1) {
+                for (auto iterador2 = next(iterador1); iterador2 != states.end(); ++iterador2) {
+                    if (find(finals.begin(), finals.end(), *iterador1) != finals.end() || 
+                        find(finals.begin(), finals.end(), *iterador2) != finals.end()) {
+                        continue;
+                    }
+                    statePairs.push_back(make_pair(*iterador1, *iterador2));
+                }
+            }
+
+            // Paso 1: Eliminar de la vectora las parejas donde hay SOLO un estado final
+            // Solucioado en el paso 0
+
+            // Paso 2: Eliminar todas las parejas que nos lleven a una pareja marcada tras recibir alguna cadena
+            
+            // Paso 3: Repetir el Paso 2 hasta que no se marquen mas parejas
+
+            // Paso 4: Llenar la vectora del atributo "equivalentStates" con las parejas no marcadas
+        }
 };
