38e54a58562722c96d234b3d124b8a405db9880d
Jeronimo Campuzano <jcampuzanc@eafit.edu.co>
2025-02-22 20:18:02 -0500

    Reader class modified with just one method, and implemented basic structure to open and read the input file

--- Reader.cpp
+++ Reader.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
 using namespace std;
+
 #include "DFA.cpp"
+
+
 
 class Reader {
     public:
-        static void read(string fileRoute){}
-        static DFA createDFA(string fileRoute){}
+
+        static vector<DFA> read(string fileRoute){
+            
+            vector<DFA> cases;  
+            int numCases;
+
+            ifstream file;
+            string line;
+
+            try {
+                file.open(fileRoute);
+
+                if (!file) {
+                    throw "Could not open the file. Please check if the txt file exists.";
+                }
+
+
+                //Procces file
+
+                //Number of cases
+                if (getline(file, line)) {
+                    numCases = stoi(line);
+                }
+
+                while (std::getline(file, line)) {
+                    cout << line << endl;
+                }
+
+                file.close();
+            } catch (const char* errorMsg) {
+                cerr << "Exception: " << errorMsg << endl;
+            } catch (...) {
+                cerr << "An unknown error occurred." << endl;
+            }
+
+
+
+            return cases;
+        }
 };
