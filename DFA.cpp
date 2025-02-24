99b433cb21e23658c5cf654d8e1f8ff9cf148fc9
Jeronimo Campuzano <jcampuzanc@eafit.edu.co>
2025-02-23 21:08:37 -0500

    Finish algorithm without any type of errors

--- DFA.cpp
+++ DFA.cpp
@@ -23,21 +23,16 @@ vector<char> DFA::getAlphabet()
     return alphabet;
 }
 
-
-void DFA::getEquivalentStates() {
+void DFA::getEquivalentStates()
+{
     cout << "Equivalent States: " << endl;
-    for (int i = 0; i < equivalentStates.size(); i++) {
+    for (int i = 0; i < equivalentStates.size(); i++)
+    {
         cout << "( " + equivalentStates[i].first + ", " + equivalentStates[i].second + " )" << endl;
     }
 }
 
-/*
-vector<tuple<string, string, string>> DFA::getTransition() {
-    return transition;
-}
-*/
-
-void DFA::findEquivalentStates() 
+void DFA::findEquivalentStates()
 {
     vector<pair<string, string>> statePairs;
     // Step 1: Create a vector filled with all the pairs that don't contain EXACTLY
@@ -55,56 +50,66 @@ void DFA::findEquivalentStates()
         }
     }
 
-    // Paso 2: Eliminar todas las parejas que nos lleven a una pareja marcada tras recibir alguna cadena
-    // Paso 3: Repetir el Paso 2 hasta que no se marquen mas parejas
+    // Step 2: Erase all the pairs that transition into another pair that isn't included
+    // into the vector 'statePairs'
     vector<pair<string, string>> prevPairs = statePairs;
-
-    // Tomar una pareja de 'statePairs', y eliminarla de prevPairs si, al aplicar la transicion, termina en alguna pareja que no este en
-    // 'statePairs'
 
     pair<string, string> pairToDisect;
 
-    do {
+    // This is done with a 'do-while' loop, that uses another vector called 'prevPairs'
+    // to store and modify the previous state of the vector 'statePairs'
+    do
+    {
         statePairs = prevPairs;
-        for (int i = 0; i < prevPairs.size(); i++) {
-
-
+        for (int i = 0; i < prevPairs.size(); i++)
+        {
 
             bool destroy = false;
-            for (int j = 0; j < alphabet.size(); j++) {
+            for (int j = 0; j < alphabet.size(); j++)
+            {
                 pairToDisect = make_pair(transitionFunction(prevPairs[i].first, alphabet[j]), transitionFunction(prevPairs[i].second, alphabet[j]));
-                if ((find(prevPairs.begin(), prevPairs.end(), pairToDisect) == prevPairs.end()) ||
-                    (find(prevPairs.begin(), prevPairs.end(), make_pair(pairToDisect.second, pairToDisect.first)) == prevPairs.end())) {
+                if ((find(prevPairs.begin(), prevPairs.end(), pairToDisect) == prevPairs.end()))
+                {
+
                     destroy = true;
-                    if (pairToDisect.first == pairToDisect.second) {
-                        destroy=false;
+
+                    if ((find(prevPairs.begin(), prevPairs.end(), make_pair(pairToDisect.second, pairToDisect.first)) != prevPairs.end()))
+                    {
+                        destroy = false;
                     }
-                    
+
+                    if (pairToDisect.first == pairToDisect.second)
+                    {
+                        destroy = false;
+                    }
+
                     break;
                 }
-            } 
-            if (destroy) {
+            }
+            if (destroy)
+            {
                 prevPairs.erase(prevPairs.begin() + i);
                 --i;
             }
-
-
-
         }
     } while (prevPairs != statePairs);
 
-    // Paso 4: Llenar el vector del atributo "equivalentStates" con las parejas no marcadas
+    // The loop will keep going as long as 'prevPairs' and 'statePairs' are different
+
+    // Step 3: Insert the remaining pairs into the attribute 'equivalentStates'
     equivalentStates = statePairs;
 }
 
-string DFA::transitionFunction(string state, char symbol) {
+string DFA::transitionFunction(string state, char symbol)
+{
     char transit;
-    for (int i = 0; i < transition.size(); i++) {
+    for (int i = 0; i < transition.size(); i++)
+    {
         transit = get<1>(transition[i])[0];
-        if (get<0>(transition[i]) == state && transit == symbol) {
+        if (get<0>(transition[i]) == state && transit == symbol)
+        {
             string newState = get<2>(transition[i]);
             return newState;
-
         }
     }
     return "Error";
