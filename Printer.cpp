a49d55add8228162bf62fc4ad97f51553a3d3e48
Jeronimo Campuzano <jcampuzanc@eafit.edu.co>
2025-02-25 14:20:41 -0500

    Add printer class to display results

---
+++ Printer.cpp
@@ -0,0 +1,11 @@
+#include "Printer.h"
+
+void Printer::printDFA(string message)
+{
+    cout << message << endl;
+}
+
+void Printer::printBar()
+{
+    cout << "--------------------------------" << endl;
+}
---
+++ Printer.h
@@ -0,0 +1,15 @@
+#ifndef PRINTER_H
+#define PRINTER_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Printer
+{
+public:
+    static void printDFA(string message);
+    static void printBar();
+};
+
+#endif
